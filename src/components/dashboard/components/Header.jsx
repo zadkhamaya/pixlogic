1af2d2e2e102c429ee26340f233b305e41df146b
Purnama S Rahayu <purnamasrahayu@gmail.com>
2023-11-29 18:52:49 +0700

    create dashboard header and handleLogout

---
+++ src/components/dashboard/components/Header.jsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { Button } from '@nextui-org/react'
+import Image from 'next/image'
+import React from 'react'
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation.js";
+
+
+export const Header = () => {
+    const router = useRouter();
+    function handleLogout() {
+        Cookies.remove("token")
+        router.push("/login")
+
+    }
+
+    return (
+        <main className=' flex justify-between p-3 border-b-2 border-violet-800'>
+            <Image src="https://www.kadencewp.com/wp-content/uploads/2020/10/alogo-2.svg" width={100} height={100} />
+            <div className=' flex w-1/5 justify-between items-center'>
+                <p>
+                    Hello, user123
+                </p>
+                <Button color='primary' onClick={handleLogout}>Logout</Button>
+            </div>
+
+        </main>
+    )
+}
