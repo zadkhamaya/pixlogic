dc6080f9ddc0c06a1cc68f1f2a673922788af98e
Purnama S Rahayu <purnamasrahayu@gmail.com>
2023-11-29 20:56:04 +0700

    create EventCard and display get events from API

--- src/components/dashboard/components/EventCard.jsx
+++ src/components/dashboard/components/EventCard.jsx
@@ -2,6 +2,7 @@ import { Button } from '@nextui-org/react'
 import Image from 'next/image'
 import React from 'react'
 
+<<<<<<< HEAD
 export const EventCard = ({ id, image, name, description, date, location, participants }) => {
   participants = participants.slice(0, 3)
   return (
@@ -9,6 +10,13 @@ export const EventCard = ({ id, image, name, description, date, location, partic
 
       <div className=' flex gap-3'>
         <Image className=' w-1/2' src={image} width={100} height={100} alt={name} />
+=======
+export const EventCard = ({ id, image, name, description, date, location }) => {
+  return (
+    <main>
+      <div className=' flex'>
+        <Image src={image} width={100} height={100} alt={name} />
+>>>>>>> 348d369 (create EventCard and display get events from API)
         <div>
           <h1 className=' font-medium'>{name}</h1>
           <p>{description}</p>
@@ -17,6 +25,7 @@ export const EventCard = ({ id, image, name, description, date, location, partic
         </div>
       </div>
 
+<<<<<<< HEAD
       {participants && <div className=' grid grid-cols-3'>
         {participants.map(participant => (
           <div key={participant.id}>
@@ -26,6 +35,8 @@ export const EventCard = ({ id, image, name, description, date, location, partic
       </div>}
       {participants.length > 0 ? <p>and others have joined this event.</p> : <p>No one has joined this event yet.</p>}
 
+=======
+>>>>>>> 348d369 (create EventCard and display get events from API)
 
     </main>
   )
