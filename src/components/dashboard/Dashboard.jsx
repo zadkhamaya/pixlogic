4871f7670eb2a92fc6011cb1fb1dff703605c19f
Purnama S Rahayu <purnamasrahayu@gmail.com>
2023-11-30 22:13:42 +0700

    edit dashboard

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -3,6 +3,8 @@ import { Header } from './components/Header'
 import { EventCard } from './components/EventCard'
 import { Button } from '@nextui-org/react'
 
+const urlImage = "https://www.kadencewp.com/wp-content/uploads/2020/10/alogo-2.svg"
+
 export const Dashboard = (events) => {
 
     const eventList = events.children
@@ -11,17 +13,17 @@ export const Dashboard = (events) => {
         <div>
             <Header />
 
-            <section className=' flex m-auto p-5 justify-center'>
-                <Button color='primary'>Create new event!</Button>
-            </section>
-
             <section className=' p-5 m-auto'>
-                <h2 className=' text-large font-semibold'>Featured Events</h2>
+                <h2 className=' text-4xl font-semibold pb-3'>My Events</h2>
                 <div className=' grid grid-cols-2 gap:1 lg:grid-cols-3 lg:gap-3'>
                     {eventList.map(({ id, image, name: eventName, description, date, location }) => {
-                        return <EventCard key={id} image={image ? image : "https://www.kadencewp.com/wp-content/uploads/2020/10/alogo-2.svg"} name={eventName} description={description} date={date} location={location}></EventCard>
+                        return <EventCard key={id} image={image && image.startsWith("http") ? image : urlImage} name={eventName} description={description} date={date} location={location}></EventCard>
                     })}
                 </div>
+            </section>
+
+            <section className=' flex m-auto p-5 justify-center'>
+                <Button color='primary'>Create new event!</Button>
             </section>
 
         </div>
--- src/components/dashboard/components/EventCard.jsx
+++ src/components/dashboard/components/EventCard.jsx
@@ -5,8 +5,8 @@ import React from 'react'
 export const EventCard = ({ id, image, name, description, date, location }) => {
   return (
     <main>
-      <div className=' flex'>
-        <Image src={image} width={100} height={100} alt={name} />
+      <div className=' flex bg-slate-300 rounded-lg'>
+        <Image className=' w-1/2' src={image} width={100} height={100} alt={name} />
         <div>
           <h1 className=' font-medium'>{name}</h1>
           <p>{description}</p>
