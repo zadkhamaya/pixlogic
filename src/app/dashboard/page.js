30520068308c951016394a530377af750b3a8fec
Purnama S Rahayu <purnamasrahayu@gmail.com>
2023-12-01 09:49:06 +0700

    add try catch fetch

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -7,9 +7,14 @@ export default async function Page() {
   const { value } = cookies().get('user')
   const user = JSON.parse(value);
   async function getEventsByUser() {
-    const res = await fetch(`${EVENTS_URL}?userid=${user.id}`, { cache: "no-store" })
-    const { data } = await res.json()
-    return data
+    try {
+      const res = await fetch(`${EVENTS_URL}?userid=${user.id}`, { cache: "no-store" })
+      const { data } = await res.json()
+      return data
+    } catch (error) {
+      console.log(error)
+    }
+
   }
 
   const events = await getEventsByUser();
