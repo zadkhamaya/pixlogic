c4aadaa42281edd965548d08887ff40dabcbc347
Purnama S Rahayu <purnamasrahayu@gmail.com>
2023-12-01 09:08:01 +0700

    set user from cookies in useEffect

--- src/components/dashboard/components/Header.jsx
+++ src/components/dashboard/components/Header.jsx
@@ -2,13 +2,14 @@
 
 import { Button } from '@nextui-org/react'
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Cookies from "js-cookie";
 import { useRouter } from "next/navigation.js";
 
 
 export const Header = () => {
     const router = useRouter();
+    const [user, setUser] = useState({})
     function handleLogout() {
         Cookies.remove("token")
         Cookies.remove("user")
@@ -16,11 +17,16 @@ export const Header = () => {
 
     }
 
-    const user = JSON.parse(Cookies.get('user'));
+    useEffect(() => {
+        const user = JSON.parse(Cookies.get('user'));
+        setUser(user)
+    }, [])
+
+
 
     return (
         <main className=' flex justify-between p-3 border-b-2 border-violet-800'>
-            <Image src="https://www.kadencewp.com/wp-content/uploads/2020/10/alogo-2.svg" alt="logo" width={100} height={100} />
+            <Image src="https://www.kadencewp.com/wp-content/uploads/2020/10/alogo-2.svg" alt="logo" width={100} height={100} priority  style={{height:'auto'}} />
             <div className=' lg:flex w-1/5 justify-between items-center'>
                 <p>
                     {user.name}
