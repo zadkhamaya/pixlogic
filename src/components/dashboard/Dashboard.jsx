ff02563d4da15268ccd57599892c855e5416dc52
Purnama S Rahayu <purnamasrahayu@gmail.com>
2023-12-01 08:09:59 +0700

    add participants

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -16,8 +16,8 @@ export const Dashboard = ({ children }) => {
             <section className=' p-5 m-auto'>
                 <h2 className=' text-4xl font-semibold pb-3'>My Events</h2>
                 <div className=' grid grid-cols-2 gap:1 lg:grid-cols-3 lg:gap-3'>
-                    {eventList && eventList.map(({ id, image, name: eventName, description, date, location }) => {
-                        return <EventCard key={id} image={image && image.startsWith("http") ? image : urlImage} name={eventName} description={description} date={date} location={location}></EventCard>
+                    {eventList && eventList.map(({ id, image, name: eventName, description, date, location, participants }) => {
+                        return <EventCard key={id} image={image && image.startsWith("http") ? image : urlImage} name={eventName} description={description} date={date} location={location} participants={participants}></EventCard>
                     })}
                 </div>
             </section>
--- src/components/dashboard/components/EventCard.jsx
+++ src/components/dashboard/components/EventCard.jsx
@@ -2,10 +2,12 @@ import { Button } from '@nextui-org/react'
 import Image from 'next/image'
 import React from 'react'
 
-export const EventCard = ({ id, image, name, description, date, location }) => {
+export const EventCard = ({ id, image, name, description, date, location, participants }) => {
+  participants = participants.slice(0, 3)
   return (
-    <main>
-      <div className=' flex bg-slate-300 rounded-lg'>
+    <main className='  bg-slate-300 rounded-lg p-3'>
+
+      <div className=' flex gap-3'>
         <Image className=' w-1/2' src={image} width={100} height={100} alt={name} />
         <div>
           <h1 className=' font-medium'>{name}</h1>
@@ -14,6 +16,15 @@ export const EventCard = ({ id, image, name, description, date, location }) => {
           <Button color='primary'>View more...</Button>
         </div>
       </div>
+
+      {participants && <div className=' grid grid-cols-3'>
+        {participants.map(participant => (
+          <div key={participant.id}>
+            <p>{participant.name}, </p>
+          </div>
+        ))}
+      </div>}
+      {participants.length > 0 ? <p>and others have joined this event.</p> : <p>No one has joined this event yet.</p>}
 
 
     </main>
